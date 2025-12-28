d13aa554b82434fb8faa1be45d52b879ce8bd47e
Alejandro <aletorregrosab@gmail.com>
2025-12-28 17:46:30 +0100

    indexación de types en sanity studio

--- sanity/schemaTypes/index.ts
+++ sanity/schemaTypes/index.ts
@@ -1,5 +1,24 @@
-import { type SchemaTypeDefinition } from 'sanity'
+import { type SchemaTypeDefinition } from "sanity";
+import { blockContent } from "./blockContent";
+import { categoryType } from "./categoryType";
+import { courseType } from "./courseType";
+import { instructorType } from "./instructorType";
+import { lessonType } from "./lessonType";
+import { moduleType } from "./moduleType";
+import { enrollmentType } from "./enrollmentType";
+import { lessonCompletionType } from "./lessonCompletionType";
+import { studentType } from "./studentType";
 
 export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [],
-}
+  types: [
+    blockContent,
+    categoryType,
+    courseType,
+    enrollmentType,
+    lessonCompletionType,
+    instructorType,
+    lessonType,
+    moduleType,
+    studentType,
+  ],
+};
